d378c9e74ee12a9242145d3672b9a69c013d31d0
dalim508 <dalim508@naver.com>
2022-01-11 11:36:16 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kangchanho:kang2468%40%40@cluster0.3o704.mongodb.net/alpha?retryWrites=true&w=majority', {
+const mongoose = require('mongoose');
+
+
+mongoose.connect(config.mongoURI, {
 
 }).then(()=>console.log('MongoDB Connected...'))
 .catch((err)=> console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!!!')
 })
+
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+
 
 
 app.listen(port, () => {
